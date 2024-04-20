49e4e49b9d75bf68bd90140709fd022f49ab45fc
AzizaNagara <azizanagara0@gmail.com>
2024-04-20 22:06:54 +0100

    Added consultation pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 // src/App.js
 import React from 'react';
-import SignUp from './components/sign_up';
+import { createRoot } from 'react-dom';
+
+
+createRoot(document.getElementById('root')).render(<App />);
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Consultation from './components/consultation'; 
+import SignUp from './components/sign_up'; 
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/consultation" element={<Consultation />} />
+        <Route path="/sign_up" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
+
+
 export default App;
+
+
