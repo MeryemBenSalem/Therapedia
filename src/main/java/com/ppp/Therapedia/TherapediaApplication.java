5e29828c453a71de3f2e4e1a7a2ab33aaca637f1
Shams Ben Mefteh <shams.benmefteh@insat.ucar.tn>
2024-04-21 16:08:13 +0100

    first commit

---
+++ src/main/java/com/ppp/Therapedia/TherapediaApplication.java
@@ -0,0 +1,13 @@
+package com.ppp.Therapedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapediaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ppp/Therapedia/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ppp.Therapedia.service;
+
+public class UserServiceImpl {
+}
