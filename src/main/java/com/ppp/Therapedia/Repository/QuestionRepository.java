985c6bfcde721ba0d1861df23ca5abe149be34db
asma <asma.zguem@insat.ucar.tn>
2024-04-22 13:42:29 +0200

    remove all files

--- src/main/java/com/ppp/Therapedia/Repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ppp.Therapedia.Repository;
-import com.ppp.Therapedia.Model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question,Integer> {
-
-}
